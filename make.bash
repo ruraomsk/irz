bc3ccc8be9de59794ea85f054e6cf953ed0ef04a
Rusinov Yury <rusinovyury@gmail.com>
2023-02-04 09:29:46 +0300

    Мелкие правки

--- make.bash
+++ make.bash
@@ -7,4 +7,6 @@ if [ $? -ne 0 ]; then
 fi
 echo 'Copy irz to device'
 scp irz root@192.168.2.1:/root
+# echo 'Copy rc.local to device'
+# scp rc.local root@192.168.2.1:/etc
 #scp test.bin admin@192.168.115.29:/home/admin
